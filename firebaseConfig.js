b9eafdaca5d9dd98e9c68cc3b423d0f4da3f5328
Your Name <princejdbacalso@gmail.com>
2025-08-27 15:42:12 +0800

    firebase config

---
+++ firebaseConfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB377ERU4n9_6HRhaOKrrcFnn0AM7A_hMU",
+  authDomain: "pvg-goal-38779.firebaseapp.com",
+  projectId: "pvg-goal-38779",
+  storageBucket: "pvg-goal-38779.firebasestorage.app",
+  messagingSenderId: "340943208976",
+  appId: "1:340943208976:web:57ad373b4ce24bf64d8095"
+};
+
+const app = initializeApp(firebaseConfig);
